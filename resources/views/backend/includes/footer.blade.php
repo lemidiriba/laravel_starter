9691ba0bd4f7b5105ad2cff602c99f050b57a59f
lemi diriba <39488278+lemidiriba@users.noreply.github.com>
2022-01-17 17:37:01 +0300

    Update footer.blade.php

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="app-footer">
     <div>
         <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
-            <a href="http://etmsoftwareplc.com">
-               ETM Software Development PLC
+            <a href="http://ld2msoftwareplc.com">
+               LD Software Development PLC
             </a>
         </strong> @lang('strings.backend.general.all_rights_reserved')
     </div>
